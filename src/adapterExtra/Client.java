282db7887288ced00fb586c7c13463dda2794865
Ankit Arora <ankit@Ankits-MacBook-Pro-2.local>
2024-05-22 08:29:33 +0530

    Added observer

--- src/adapterExtra/Client.java
+++ src/adapterExtra/Client.java
@@ -1,5 +1,10 @@
 package adapterExtra;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
 public class Client {
     public static void main(String[] args) {
         ITarget iTarget = new Adapter();
